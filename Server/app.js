7cfeb8825c224dc45fcea5d060f00d0eeb7b93cc
Gowthamaad <gowtham03gmv@gmail.com>
2025-03-07 12:56:39 +0530

    CORS modified for deployment

--- Server/app.js
+++ Server/app.js
@@ -29,6 +29,29 @@ app.use(
   })
 );
 
+app.use((req, res, next) => {
+  res.setHeader(
+    "Access-Control-Allow-Origin",
+    "https://personalized-fitness-class-booking-pl.netlify.app"
+  );
+  res.setHeader(
+    "Access-Control-Allow-Methods",
+    "GET, POST, PUT, DELETE, OPTIONS"
+  );
+  res.setHeader("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  next();
+});
+
+app.options("*", (req, res) => {
+  res.header(
+    "Access-Control-Allow-Origin",
+    "https://personalized-fitness-class-booking-pl.netlify.app"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
+  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
+  res.sendStatus(200);
+});
+
 app.use(cookieParser());
 
 app.use("/user", userRoutes);
