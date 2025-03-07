2f3ebb305b7d7b997cd889652e8d9f3c238eedca
Gowthamaad <gowtham03gmv@gmail.com>
2025-03-07 06:07:35 +0530

    deployment setup for backend

--- Server/app.js
+++ Server/app.js
@@ -11,9 +11,21 @@ const paymentRoutes = require("./routes/paymentRoutes");
 
 const app = express();
 
+const allowedOrigins = [
+  "http://localhost:5173", // Local dev frontend
+  "https://personalized-fitness-class-booking-pl.netlify.app/", // Production frontend URL
+];
+
 app.use(
   cors({
-    origin: "https://personalized-fitness-class-booking-pl.netlify.app/",
+    origin: (origin, callback) => {
+      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
+        // Allow requests from allowed origins or if no origin is present (e.g., for Postman requests)
+        callback(null, true);
+      } else {
+        callback(new Error("Not allowed by CORS"));
+      }
+    },
     credentials: true,
     methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
