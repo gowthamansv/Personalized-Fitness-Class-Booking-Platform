bb29d48c2ce4060de4449e2819235d1953b733a8
Gowthamaad <gowtham03gmv@gmail.com>
2025-03-07 06:14:55 +0530

    deployment setup for backend

--- Server/app.js
+++ Server/app.js
@@ -19,37 +19,19 @@ const allowedOrigins = [
 app.use(
   cors({
     origin: (origin, callback) => {
-      if (allowedOrigins.indexOf(origin) !== -1 || !origin) {
-        // Allow requests from allowed origins or if no origin is present (e.g., for Postman requests)
+      if (allowedOrigins.includes(origin) || !origin) {
         callback(null, true);
       } else {
         callback(new Error("Not allowed by CORS"));
       }
     },
-    credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
-    allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
 
+app.options("*", cors());
+
 app.use(cookieParser());
 app.use(express.json());
-
-app.use((req, res, next) => {
-  res.header(
-    "Access-Control-Allow-Origin",
-    "https://personalized-fitness-class-booking-pl.netlify.app/"
-  );
-  res.header("Access-Control-Allow-Credentials", "true");
-  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, OPTIONS");
-  res.header("Access-Control-Allow-Headers", "Content-Type, Authorization");
-
-  if (req.method === "OPTIONS") {
-    return res.sendStatus(204);
-  }
-
-  next();
-});
 
 app.use("/user", userRoutes);
 app.use("/trainer", trainerRoutes);
